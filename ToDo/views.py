1d87d932b3d9982e2807f4eb3f8831024a4806f7
Vishagh Sivankutty <vishaghsivankutty11@gmail.com>
2025-08-19 19:21:52 +0530

    done

--- ToDo/views.py
+++ ToDo/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from task.models import Task
 
 def home(request):
-    return render(request,'home.html')
+    t = Task.objects.filter(is_completed=False).order_by('updated_at')
+    ct = Task.objects.filter(is_completed=True)
+    context = {
+        "tasks" : t,
+        "ct" : ct,
+    }
+    return render(request,'home.html',context)
